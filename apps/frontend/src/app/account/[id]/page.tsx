3d9701525e4290d782070a3d6186a5dac0f85ff4
cuonglv <lamcuong1611@gmail.com>
2024-12-19 11:43:20 +0700

    test

--- apps/frontend/src/app/account/[id]/page.tsx
+++ apps/frontend/src/app/account/[id]/page.tsx
@@ -3,11 +3,7 @@ import AccountDetail from '@expense-management/frontend/container/detail';
 type DetailProps = {};
 
 const Detail: React.FC<DetailProps> = () => {
-  return (
-    <>
-      <AccountDetail />
-    </>
-  );
+  return <>{/* <AccountDetail /> */}</>;
 };
 
 export default Detail;
