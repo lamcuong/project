d30500575f697ccbf6503c20c224a8e90cf404e4
cuonglv <lamcuong1611@gmail.com>
2025-01-20 17:17:49 +0700

    remove unused api folder

--- apps/frontend/src/app/api/config.ts
+++
@@ -1,6 +0,0 @@
-import { env } from '@expense-management/frontend/config/env';
-
-const baseURL = env.BASE_URL;
-export const apiUser = `${baseURL}/user`;
-export const apiAccount = `${baseURL}/account`;
-export const apiExpense = `${baseURL}/expense`;
